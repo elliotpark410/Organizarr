b6cd31ee3f1197c47cafb88cdb1ac3c625167ee6
Leslie Patino <lesliejpatino@gmail.com>
2022-02-10 22:14:58 -0800

    completed User model

--- models/user.js
+++ models/user.js
@@ -7,3 +7,54 @@ class User extends Model {
         return bcrypt.compareSync(loginPw, this.password);
     }
 }
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        subject: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        hooks: {
+          beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+          beforeUpdate: async (updatedUserData) => {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+      }
+    );
+    
+    module.exports = User;
