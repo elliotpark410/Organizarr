a1f76c59f51ce865f29bc28871fd0f3ddcaa0d40
Leslie Patino <lesliejpatino@gmail.com>
2022-02-11 10:31:03 -0800

    referenced all models in the seed.js file

---
+++ seeds/seed.js
@@ -0,0 +1,6 @@
+const sequelize = require('../config/connection');
+const { User } = require('../models/user');
+const { ToDo } = require('../models/toDo');
+const { Subject } = require('../models/subject');
+const { StudyGroup} = require('../models/studyGroup');
+const { Notes } = require('../models/notes');
