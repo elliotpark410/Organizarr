b645d19eb1d415c9ad682d0b7723da9a3c0d7d50
Leslie Patino <lesliejpatino@gmail.com>
2022-02-11 10:34:37 -0800

    referenced all seed files in the seed.js file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,6 +1,15 @@
 const sequelize = require('../config/connection');
+
+// Referencing all models
 const { User } = require('../models/user');
 const { ToDo } = require('../models/toDo');
 const { Subject } = require('../models/subject');
 const { StudyGroup} = require('../models/studyGroup');
 const { Notes } = require('../models/notes');
+
+// Referencing all seed files
+const userData = require('./userData');
+const toDoData = require('./toDoData');
+const subjectData = require('./subjectData');
+const studyGroupData = require('./studyGroupData');
+const notesData = require('./notesData');
