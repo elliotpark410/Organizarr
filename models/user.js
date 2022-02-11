c367c7140c9980d8aaad0542f0add573babc4904
Elliot Park <elliotpark410@gmail.com>
2022-02-10 23:27:25 -0800

    review models and remove unnecessary code

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const { Model, Data } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
