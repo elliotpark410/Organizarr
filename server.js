16241f874c8f297792bcb2e64ab5b6181a9e62c4
Leslie Patino <lesliejpatino@gmail.com>
2022-02-10 21:06:05 -0800

    updated server.js file

--- server.js
+++ server.js
@@ -24,4 +24,20 @@ const sess = {
     store: new SequelizeStore({
       db: sequelize
     })
-  };
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
