912220fe6d4dd9e30051a8e88bd5542f4b6def26
Leslie Patino <lesliejpatino@gmail.com>
2022-02-11 10:41:12 -0800

    created a function to seed the database

--- seeds/seed.js
+++ seeds/seed.js
@@ -13,3 +13,16 @@ const toDoData = require('./toDoData');
 const subjectData = require('./subjectData');
 const studyGroupData = require('./studyGroupData');
 const notesData = require('./notesData');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    process.exit(0);
+}
+
+seedDatabase();
