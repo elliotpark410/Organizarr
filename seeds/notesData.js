d43a38143484eda23038ec05ea1670084e8a5b43
Leslie Patino <lesliejpatino@gmail.com>
2022-02-11 10:26:40 -0800

    created placeholder seed data for the Notes model

---
+++ seeds/notesData.js
@@ -0,0 +1,18 @@
+[
+    {
+        "title": "VS Code Shortcuts",
+        "description": "Cmnd+D allows you to select the next occurence of the word you have selected. Opt+Shift+F lets you autoformat your file"
+    },
+    {
+        "title": "HTML/CSS Reminders",
+        "description": "Remember to SAVE! in HTML, the link to the stylesheet goes in the head. To select all elements in a page while in CSS, use the *."
+    },
+    {
+        "title": "Javascript Reminders",
+        "description": "Opt+Shift+F lets you autoformat your file. Variables saved under var can be reassigned but the ones under const cannot."
+    },
+    {
+        "title": "JS Datatypes",
+        "description": "'This is a string' ['This', 'is', 'an', 'array']"
+    },
+]
