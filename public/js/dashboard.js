1526317db9a7e59d0e5e98b33496461ec41539d6
705H-S <93358442+705h-S@users.noreply.github.com>
2022-02-12 15:00:44 -0800

    created function to ADD a todo

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -19,18 +19,42 @@ const delToDO = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/"filler"/${id}`, {
+      const response = await fetch(`/BLANK/${id}`, {
         method: 'DELETE',
       });
   
       if (response.ok) {
-        document.location.replace('/"filler"');
+        document.location.replace('/BLANK');
       } else {
         alert('Failed to delete Note');
       }
     }
   };
+
+  const newToDo = async (event) => {
+    event.preventDefault();
   
+    const desc = document.querySelector('NEED INPUT FORM').value.trim();
+    if (desc) {
+      const response = await fetch(`/BLANK`, {
+        method: 'POST',
+        body: JSON.stringify({ description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/BLANK');
+      } else {
+        alert('Failed to create toDO');
+      }
+    }
+  };
+
+
+  
+
   document
   .getElementById('#deleteToDo')
   .addEventListener('click', delToDO);
@@ -38,3 +62,7 @@ const delToDO = async (event) => {
   document
   .getElementById('#deleteNote')
   .addEventListener('click', delNotes);
+
+  document
+  .querySelector('NEED CLASS')
+  .addEventListener('submit', newToDo);
