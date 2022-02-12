3897ab97b66a4ffad185690a4139b6a87ef954a0
Elliot Park <elliotpark410@gmail.com>
2022-02-12 03:00:00 -0800

    update name column in subject model and subject seed file

---
+++ controllers/api/studyGroupRoutes.js
@@ -0,0 +1,107 @@
+const router = require('express').Router();
+const { Subject, StudyGroup } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const studyGroupData = await StudyGroup.findAll({
+      include: [
+        {
+          model: subject,
+          attributes: ['url', 'time', 'studyPreference'],
+        },
+      ],
+    });
+    
+    const subject = subjectData.get({ plain: true });
+
+    res.render('subject', {
+      ...subject,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    const subjectData = await Subject.findByPk(req.params.id, {
+      include: [
+        {
+          model: StudyGroup,
+          attributes: ['url', 'time', 'studyPreference'],
+        },
+      ],
+    });
+
+    const subject = subjectData.get({ plain: true });
+
+    res.render('todo', {
+      ...subject,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newSubject = await Subject.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newSubject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const subjectData = await Subject.update({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!subjectData) {
+      res.status(404).json({ message: 'No subject found with this id!' });
+      return;
+    }
+
+    res.status(200).json(subjectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const subjectData = await Subject.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!subjectData) {
+      res.status(404).json({ message: 'No subject found with this id!' });
+      return;
+    }
+
+    res.status(200).json(subjectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
