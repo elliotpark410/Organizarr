861d5dbbcb16b3f9e0e59c7277c2ef250b6153e7
705H-S <93358442+705h-S@users.noreply.github.com>
2022-02-12 00:31:38 -0800

    added a delete note function

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -14,7 +14,27 @@ const delToDO = async (event) => {
       }
     }
   };
+// Delete a note
+  const delNotes = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/"filler"/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/"filler"');
+      } else {
+        alert('Failed to delete Note');
+      }
+    }
+  };
   
   document
   .getElementById('#deleteToDo')
   .addEventListener('click', delToDO);
+
+  document
+  .getElementById('#deleteNote')
+  .addEventListener('click', delNotes);
