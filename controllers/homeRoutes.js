6c4afbeda3a0bd7a48556dfb3545b4dea1ffa5df
Elliot Park <elliotpark410@gmail.com>
2022-02-12 02:12:11 -0800

    add route for user

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,71 @@
+// Express router is a class which helps us to create router handlers e.g. get, post, delete, put routes
 const router = require('express').Router();
-const { User, Notes, ToDo, Subject, StudyGroup } = require('../models');
+
+// Export User model
+const { User, ToDo, Subject, StudyGroup, Notes } = require('../models');
+
 const withAuth = require('../utils/auth');
 
+// If user has session saved, then user will automatically be logged in
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
 
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: ToDo }, { model: Notes }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// Use withAuth middleware to prevent access to route
+router.get('/studygroup', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Subject }, { model: StudyGroup }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('studygroup', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// when they route to signup page, it will render singup.handlebars
 router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -13,4 +76,5 @@ router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
+// Export module
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
+// Express router is a class which helps us to create router handlers e.g. get, post, delete, put routes
 const router = require('express').Router();
 
+// exports code to /api and /homeRoutes files
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
+// router.use (path, function)
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
+// Export module
 module.exports = router;
