f007b74a2b13ac19377033db6fecd062aab83f74
705H-S <93358442+705h-S@users.noreply.github.com>
2022-02-13 15:18:52 -0800

    added console.log for test purposes

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
   
     if (email && password) {
       // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/api/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
@@ -16,8 +16,10 @@ const loginFormHandler = async (event) => {
       if (response.ok) {
         // If successful, redirect the browser to the profile page
         document.location.replace('/dashboard');
+        console.log("sign in succesfull")
       } else {
         alert(response.statusText);
+        console.log("sign in fail")
       }
     }
   };
@@ -45,10 +47,10 @@ const loginFormHandler = async (event) => {
   };
   
   document
-    .querySelector('.login')
+    .querySelector('#loginBtn')
     .addEventListener('submit', loginFormHandler);
   
   document
-    .querySelector('.signup')
+    .querySelector('#signUpBtn')
     .addEventListener('submit', signupFormHandler);
   
